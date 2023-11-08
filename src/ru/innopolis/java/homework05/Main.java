9587a53d38b42673900d818b1625435d6a2e89b8
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-11-08 19:06:59 +0300

    fix console input with scanner

--- src/ru/innopolis/java/homework05/Main.java
+++ src/ru/innopolis/java/homework05/Main.java
@@ -1,5 +1,6 @@
 package ru.innopolis.java.homework05;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,17 +14,31 @@ public class Main {
         for (int i = minCount; i < maxCount; i++) {
             System.out.println("Введите имя");
             String name = scanner.nextLine();
-            System.out.println("Введите подразделение");
-            int departmentNumber = scanner.nextInt();
-            System.out.println("Введите квалификацию от 0 до 100");
-            int qualityLevel = scanner.nextInt();
-            System.out.println("Участвует ли сотрудник в проекте? Да или Нет");
-            boolean isProjectWorker = scanner.nextLine() == "Да";
+            try {
+                System.out.println("Введите подразделение");
+                int departmentNumber = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Введите квалификацию от 0 до 100");
+                int qualityLevel = scanner.nextInt(); // берет только число, не переходя на другую строку
+                scanner.nextLine();
+                System.out.println("Участвует ли сотрудник в проекте? Да или Нет");
+                String isProjectWorkerStr = scanner.nextLine();
+                boolean isProjectWorker = isProjectWorkerStr.equals("Да"); // сравнивать по equals
 
-            Person newPerson = new Person(name, departmentNumber, qualityLevel, false);
+                System.out.println("Участвует ли сотрудник в проекте? true или false");
+                boolean isProjectWorkerTest = scanner.nextBoolean();
+                scanner.nextLine();
+                System.out.println(isProjectWorkerTest);
+                Person newPerson = new Person(name, departmentNumber, qualityLevel, isProjectWorker);
 
-            persons[persons.length - 1] = newPerson;
-            System.out.println(newPerson);
+                persons[i] = newPerson; //0, 1
+                System.out.println(newPerson);
+            } catch (InputMismatchException exception) {
+                System.err.printf("Ошибка %s\n", exception);
+                System.out.println("Программа завершена");
+            } finally {
+                System.out.printf("Итерация %d завершена\n", i);
+            }
         }
 
 
