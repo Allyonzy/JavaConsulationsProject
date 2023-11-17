1aa3cc6b76b2bfac7182f42b9dc8fd33801029b3
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-11-17 11:53:52 +0300

    Доработки

---
+++ src/ru/innopolis/java/homework07/App.java
@@ -0,0 +1,7 @@
+package ru.innopolis.java.homework07;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/innopolis/java/homework07/DiscountProduct.java
@@ -0,0 +1,11 @@
+package ru.innopolis.java.homework07;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class DiscountProduct extends Product {
+    private int discount;
+    private LocalDate discountLastDay = LocalDate.now();
+
+
+}
