21e3ff067884590b8a9c19f2e9937d9f0091bba2
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-12-21 19:44:02 +0300

    Длинное решение с учетом размерности массива

---
+++ homework11/src/ru/innopolis/java/lambdas/Sequence.java
@@ -0,0 +1,27 @@
+package ru.innopolis.java.lambdas;
+
+public class Sequence {
+    public int[] filter (int[] inputNumbers, ByCondition condition) {
+        boolean[] resultConditions = new boolean[inputNumbers.length];
+
+        int size = 0;
+
+        for (int pos = 0; pos < inputNumbers.length; pos++) {
+            resultConditions[pos] = condition.isOk(inputNumbers[pos]);
+            if(resultConditions[pos]) {
+                size++;
+            }
+        }
+
+        int[] resultNumbers = new int[size];
+
+        int count = 0;
+        for (int pos = 0; pos < resultConditions.length; pos++) {
+            if(resultConditions[pos]) {
+                resultNumbers[count] = inputNumbers[pos];
+                count++;
+            }
+        }
+        return resultNumbers;
+    }
+}
